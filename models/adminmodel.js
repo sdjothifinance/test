0d67cbe3f9e11ef13cb795a2a2df8298fee100e9
sdjothifinance <127538739+sdjothifinance@users.noreply.github.com>
2023-03-25 21:49:39 +0530

    kk

--- models/adminmodel.js
+++ models/adminmodel.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    address:{
+        type:String,
+        required:true
+    },
     userid:{
         type:String
     },
